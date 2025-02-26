482efe1503d3c4d9e5abc8b2573f98a2902871a8
Jan Tytgat <jan.tytgat@corelayer.eu>
2025-02-26 15:55:27 +0100

    Refactor Event
    Signed-off-by: Jan Tytgat <jan.tytgat@corelayer.eu>

--- pkg/labor/event.go
+++ pkg/labor/event.go
@@ -1,7 +1,26 @@
 package labor
 
-type Event string
+import "log/slog"
+
+const (
+	laborEventCategory = "labor"
+)
+
+type Event struct {
+	Category string
+	Type     string
+	Message  string
+}
+
+func (e Event) LogValue(sender string) slog.Attr {
+	return slog.Group(
+		"event",
+		slog.String("sender", sender),
+		slog.String("category", e.Category),
+		slog.String("type", e.Type),
+	)
+}
 
 func (e Event) String() string {
-	return string(e)
+	return e.Message
 }
--- pkg/labor/event_test.go
+++ pkg/labor/event_test.go
@@ -1,22 +1,65 @@
 package labor
 
-import "testing"
+import (
+	"log/slog"
+	"reflect"
+	"testing"
+)
 
-func TestEvent_String(t *testing.T) {
+func TestEvent_LogValue(t *testing.T) {
+	type args struct {
+		sender string
+	}
 	tests := []struct {
-		name string
-		e    Event
-		want string
+		name  string
+		event Event
+		args  args
+		want  slog.Attr
 	}{
 		{
-			name: "simple",
-			e:    Event("simple"),
-			want: "simple",
+			name:  "message",
+			event: Event{Message: "message"},
+			args:  args{"sender"},
+			want: slog.Group("event",
+				slog.String("sender", "sender"),
+				slog.String("category", ""),
+				slog.String("type", "")),
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.e.String(); got != tt.want {
+			if got := tt.event.LogValue(tt.args.sender); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("LogValue() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestEvent_String(t *testing.T) {
+	tests := []struct {
+		name  string
+		event Event
+		want  string
+	}{
+		{
+			name:  "message",
+			event: Event{Message: "message"},
+			want:  "message",
+		},
+		{
+			name:  "operator",
+			event: operatorStartedEvent,
+			want:  "operator started",
+		},
+		{
+			name:  "scheduler",
+			event: schedulerStartedEvent,
+			want:  "scheduler started",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.event.String(); got != tt.want {
 				t.Errorf("String() = %v, want %v", got, tt.want)
 			}
 		})
